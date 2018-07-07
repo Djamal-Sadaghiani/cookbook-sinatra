f2c1c93dcf11a2e89a67103a42d1cadef513375c
Djamal Sadaghiani <dj8amal@gmail.com>
2018-07-07 21:24:30 +0800

    finished and cleaned up the project

--- app.rb
+++ app.rb
@@ -17,14 +17,6 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
-get '/about' do
-  erb :about
-end
-
-get '/team/:username' do
-  puts params[:username]
-  "The username is #{params[:username]}"
-end
 
 get '/' do
   @cookbook = cookbook.all
